d92e532cb94cadfd36b6e0dd2316ea193f5acff3
zernov <gzernov@gmail.com>
2016-04-27 01:53:25 +0300

    update-27.04-1:53am

--- server.R
+++ server.R
@@ -5,14 +5,48 @@ library(shinydashboard)
 
 shinyServer(function(input, output) {
   
+  output$arcsFromManuallyUI <- renderUI({
+    plotManually <- renderPlot({
+      graph <- empty.graph(as.character(c(1:input$nodesManually)))
+    })
+    arcsFromManuallyUI <- selectInput("arcsManuallyFrom", "From", c(1:input$nodesManually))
+  })
+  
+  output$arcsToManuallyUI <- renderUI({
+    selectInput("arcsManuallyTo", "To", c(1:input$nodesManually))
+  })
+  
+  nodesManually <- eventReactive(input$nodesManually, {
+    write(as.character(c(1:input$nodesManually)), "graphManuallyNodes.txt")
+    write("","graphManuallyArcs.txt")
+  })
+  
+  arcsManually <- eventReactive(input$arcsAddManually, {
+    nodes <- scan(file = "graphManuallyNodes.txt", what = character())
+    arcs <- scan(file = "graphManuallyArcs.txt", what = character())
+    graph <- empty.graph(nodes)
+    if (input$arcsManuallyTo != input$arcsManuallyFrom) {
+      arcs(graph) <- matrix(c(arcs, c(input$arcsManuallyFrom, input$arcsManuallyTo)), ncol=2, byrow = TRUE, dimnames = list(c(),c("from", "to")))
+    } else {
+      arcs(graph) <- matrix(arcs, ncol=2, byrow = TRUE, dimnames = list(c(),c("from", "to")))
+    }
+    write(as.vector(t(arcs(graph))), "graphManuallyArcs.txt")
+  })
+  
+  output$plotManually <- renderPlot({
+    nodesManually()
+    arcsManually()
+    nodes <- scan(file = "graphManuallyNodes.txt", what = character())
+    arcs <- scan(file = "graphManuallyArcs.txt", what = character())
+    graph <- empty.graph(nodes)
+    arcs(graph) <- arcs
+    plot(graph)
+  })
+  
   output$plotExample <- renderPlot({
     dag <- empty.graph(names(marks))
     arcs(dag) <- matrix(c("VECT", "MECH", "ALG","MECH", "ALG","VECT", "ANL", "ALG", "STAT","ALG","STAT","ANL"), ncol=2, byrow = TRUE, dimnames=list(c(),c("from","to")))
-    graphviz.plot(dag)
-  })
-  
-  output$plotCreate <- renderPlot({
-      
+    plot(dag)
   })
   
   output$plotUpload <- renderPlot({
--- ui.R
+++ ui.R
@@ -3,90 +3,112 @@ library(bnlearn)
 library(Rgraphviz)
 library(shinydashboard)
 
-header <- dashboardHeader(title = "Bayesian networks in R")
+header <- dashboardHeader(title = "Bayesian Networks")
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Overview", tabName = "overview", selected = TRUE),
-    menuItem("Basics", tabName = "basics"),
-    menuItem("Try for youself", tabName = "tryforyourself",
-             menuSubItem("How to", tabName = "tryforyourselfhowto"),
-             menuSubItem("Create", tabName = "tryforyourselfcreate"),
-             menuSubItem("Upload", tabName = "tryforyourselfupload")
+    menuItem("Basics", tabName = "basics",
+             menuSubItem("Graph theory", tabName = "basicsgraphtheory"),
+             menuSubItem("Introduction to R", tabName = "basicsintroductiontor"),
+             menuSubItem("Exercises", tabName = "basicsexercises")
+    ),
+    menuItem("Creating graph", tabName = "creating",
+             menuSubItem("How to", tabName = "creatinghowto"),
+             menuSubItem("Manually", tabName = "creatingmanually"),
+             menuSubItem("Learning", tabName = "creatinglearning")
     )
   )
 )
 
 body <- dashboardBody(
+  withMathJax(),
+  tags$script("
+                          MathJax.Hub.Config({
+                          tex2jax: {
+                          inlineMath: [['$','$']],
+                          processEscapes: true
+                          }
+                          });"
+  ),
   tabItems(
     #OVERVIEW
     tabItem(tabName = "overview",
             fluidPage(
               titlePanel("Overview"),
-              p(
-                strong("Abstract"), " Data recorded across multiple variables of interest for a given phenomenon often do not contain any explicit temporal information.
+              p(strong("Abstract"), " Data recorded across multiple variables of interest for a given phenomenon often do not contain any explicit temporal information.
                 In the absence of such information, the data essentially represent a static snapshot of the underlying phenomenon at a particular moment in time. 
-                For this reason, they are sometimes referred to as ", em("static data"), "."),
-              p(
-                "Static Bayesian networks, commonly known simply as Bayesian networks, provide an intuitive and comprehensive framework to model the dependencies between the variables in static data. 
-                ."
+                For this reason, they are sometimes referred to as ", strong(em("static data")), "."),
+              p("Static Bayesian networks, commonly known simply as Bayesian networks, provide an intuitive and comprehensive framework to model the dependencies between the variables in static data."),
+              p("In this application a brief introduction to the terminology and the basic properties of graphs, with particular attention to directed graphs, is provided."),
+              p("A brief introduction to the $R$ environment and basir $R$ programming is also provided.")
               )
-              )
-              )
-    ,
-    #BAICS
-    tabItem(tabName = "basics",
+            ),
+    #BASICS:GRAPHTHEORY
+    tabItem(tabName = "basicsgraphtheory",
             fluidPage(
-              titlePanel("Basics"),
-              p(
-                strong("Bayesian networks"), " are a class of graphical models that allow a concise representation of the probabilistic dependecies between a given set of random variables as a directed acyclic graph. 
-                Each node corresponds to a random variable."),
-              p("Consider a data set consisting of the exam scores of 88 students across five different topics, namely, ", strong(em("mechanics, ")), strong(em("vectors, ")), strong(em("algebra, ")), strong(em("analysis, ")), "and ", strong(em("statistics."))),
-              plotOutput("plotExample")
+              titlePanel("Graph theory"),
+              h3("Graph, Nodes, and Arcs"),
+              p("A graph $G = (V, A)$ consists of a nonempty set $V$ of ", strong(em("nodes")), " or ", strong(em("vertices")), "and a finite (but possibly empty) set $A$ of pairs of vertices called ", strong(em("arcs")), ", ", strong(em("links")), ", or ", strong(em("edges")), "."),
+              p("Each arc $a = (u, v)$ can be defined either as an ordered or an unordered pair of nodes, which are said to be ", strong(em("connected")), "by and ", strong(em("incident")), " on the arc and to be ", strong(em("adjacent")), "to each other.
+                ")
               )
     ),
-    #HOWTO
-    tabItem(tabName = "tryforyourselfhowto",
+    #CREATING:HOWTO
+    tabItem(tabName = "creatinghowto",
             fluidPage(
               titlePanel("How to"),
-              p("some text")
-            )
-    ),
-    #CREATE
+              p("Consider a data set consisting of the exam scores of 88 students across five different topics, namely, mechanics, vectors, algebra, analysis, and statistics.
+                The scores are bounded in the interval [0, 100].
+                This data set was originally investigated by Mardia. et al. (1979) and subsequently in classic books on graphical models such as Whittaker (1990) and Edwards (2000).
+                A copy of the data is included in ", strong("bnlearn"), " under the name ", code("marks"), "."),
+              pre(includeText("examples/creating01")),
+              p("Upon loading the data, we can create an empty network with the nodes corresponding to the variables in ", code("marks"), " using the ", code("empty.graph"), "function."),
+              pre(includeText("examples/creating02"))
+              )
+            ),
+    #CREATING:MANUALLY
     tabItem(
-      tabName = "tryforyourselfcreate",
+      tabName = "creatingmanually",
       fluidPage(
-        titlePanel("Create"),
+        titlePanel("Manually"),
         sidebarLayout(
           sidebarPanel(
-            #fileInput("upload", "Choose a file:", accept = c(".txt"))
+            sliderInput("nodesManually", 
+                        "Number of nodes:", 
+                        min = 1,
+                        max = 10, 
+                        value = 5),
+            uiOutput("arcsFromManuallyUI"),
+            uiOutput("arcsToManuallyUI"),
+            actionButton("arcsAddManually", "Add")
           ),
           mainPanel(
-            plotOutput("plotCreate"),
-            tableOutput("tableArcsCreate")
+            plotOutput("plotManually"),
+            tableOutput("tableArcsManually")
           )
         )
       )
     ),
-    #UPLOAD
+    #CREATING:LEARNING
     tabItem(
-      tabName = "tryforyourselfupload",
+      tabName = "creatinglearning",
       fluidPage(
-        titlePanel("Upload"),
+        titlePanel("Learning"),
         sidebarLayout(
           sidebarPanel(
-            fileInput("upload", "Choose a file:", accept = c(".txt"))
+            fileInput("uploadLearning", "Choose a file:", accept = c(".txt"))
           ),
           mainPanel(
-            plotOutput("plotUpload"),
+            plotOutput("plotLearning"),
             box(
-              tableOutput("tableArcsUpload")
+              tableOutput("tableArcsLearning")
             )
           )
         )
       )
     )
-              )
-              )
+    )
+)
 
 dashboardPage(header, sidebar, body)
